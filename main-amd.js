43a587a0d05a8c162c680d254e7dcd2905a5de24
Matthew Phillips <matthew@matthewphillips.info>
2015-03-11 14:57:17 -0400

    Updated to use can/steal
    This updates to the latest can and stealjs. Everything works except for
    Browserify which works if you apply the patch I created. Waiting on that
    PR to be merged and a new release.

--- main-amd.js
+++ main-amd.js
@@ -1,8 +1,8 @@
-define(["can", "jquery", "can/view/stache","can-tabs"], function(can, $){
-	var template = can.stache("<h2>can-tabs</h2><can-tabs>"+
+define(["can", "jquery", "can/view/stache","bit-tabs"], function(can, $){
+	var template = can.stache("<h2>bit-tabs</h2><bit-tabs>"+
 		"<can-panel title='CanJS'>CanJS provides the MV*</can-panel>"+
 		"<can-panel title='StealJS'>Steal provides the build</can-panel>"+
-	"</can-tabs>");
+	"</bit-tabs>");
 
 	$("body").append(template());
 });
--- main-browserify.js
+++ main-browserify.js
@@ -1,8 +1,8 @@
-require("can-tabs");
-require("canjs/view/stache/stache");
-var can = require("canjs");
+require("bit-tabs");
+require("can/view/stache/stache");
+var can = require("can");
 var $ = require("jquery");
 
-var template = can.stache("<h2>can-tabs</h2><can-tabs><can-panel title='CanJS'>CanJS provides the MV*</can-panel><can-panel title='StealJS'>StealJS provides the infrastructure.</can-panel></can-tabs>")
+var template = can.stache("<h2>bit-tabs</h2><bit-tabs><can-panel title='CanJS'>CanJS provides the MV*</can-panel><can-panel title='StealJS'>StealJS provides the infrastructure.</can-panel></bit-tabs>")
 
 $("body").append(template());
