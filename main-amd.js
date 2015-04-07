70834ae6806cebef30c97c32972bcd1465b85ffd
Matthew Phillips <matthew@matthewphillips.info>
2015-04-07 09:17:56 -0400

    Use latest bit-tabs

--- main-amd.js
+++ main-amd.js
@@ -1,7 +1,7 @@
 define(["can", "jquery", "can/view/stache","bit-tabs"], function(can, $){
 	var template = can.stache("<h2>bit-tabs</h2><bit-tabs>"+
-		"<can-panel title='CanJS'>CanJS provides the MV*</can-panel>"+
-		"<can-panel title='StealJS'>Steal provides the build</can-panel>"+
+		"<bit-panel title='CanJS'>CanJS provides the MV*</bit-panel>"+
+		"<bit-panel title='StealJS'>Steal provides the build</bit-panel>"+
 	"</bit-tabs>");
 
 	$("body").append(template());
--- main.js
+++ main.js
@@ -3,6 +3,6 @@ require("can/view/stache/stache");
 var can = require("can");
 var $ = require("jquery");
 
-var template = can.stache("<h2>can-tabs</h2><can-tabs><can-panel title='CanJS'>CanJS provides the MV*</can-panel><can-panel title='StealJS'>StealJS provides the infrastructure.</can-panel></can-tabs>")
+var template = can.stache("<h2>can-tabs</h2><can-tabs><bit-panel title='CanJS'>CanJS provides the MV*</bit-panel><bit-panel title='StealJS'>StealJS provides the infrastructure.</bit-panel></can-tabs>")
 
 $("body").append(template());
