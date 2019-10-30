03339f87c95d5892e81f801de6d0f0664840f2e9
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-10-30 16:22:07 +0100

    Landscaper: Migrate to Qunit2

--- test-steal.js
+++ test-steal.js
@@ -9,18 +9,18 @@ require("can/view/stache/stache");
 F.attach(QUnit);
 
 QUnit.module("bit-tabs", {
-  setup: function(){
+  beforeEach: function(assert) {
     var template = can.stache("<h2>can-tabs</h2><bit-tabs><bit-panel title='CanJS'>CanJS provides the MV*</bit-panel><bit-panel title='StealJS'>StealJS provides the infrastructure.</bit-panel></bit-tabs>")
 
     $("body").append(template());
   }
 });
 
-test("Basics", function(){
+QUnit.test("Basics", function(assert) {
   F("bit-tabs").exists("There is bit-tabs");
 });
 
-test("Clicking something", function(){
+QUnit.test("Clicking something", function(assert) {
   F("bit-tabs li:nth(1)").click();
   F("bit-panel:nth(1)").text("StealJS provides the infrastructure.", "Clicking works");
 });
